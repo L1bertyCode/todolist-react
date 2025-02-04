c0e5937aef9ba369811b5315e02f4e9a6db5e2fa
LC <libertycodeg@gmail.com>
2025-02-04 19:55:36 +0300

    cr pr

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { TodolistItem } from './TodolistItem/TodolistItem';
+
+function App() {
+  return (
+    <div className="app">
+      <TodolistItem />
+      <TodolistItem />
+      <TodolistItem />
+    </div>
+  );
+}
+
+export default App;
